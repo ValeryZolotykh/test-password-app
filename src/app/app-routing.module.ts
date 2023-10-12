7c2e9657d999dfc59675b3f199ba59efd83998bd
Valeriia Zolotykh <98551132+ValeryZolotykh@users.noreply.github.com>
2023-10-12 11:59:08 +0300

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
-
-const routes: Routes = [
-  { path: '', component: AppComponent },
-];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
