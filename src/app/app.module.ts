d61142d6ddba3385e0182028566d8f89375a4a1c
Valeriia Zolotykh <98551132+ValeryZolotykh@users.noreply.github.com>
2023-10-12 11:51:55 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
