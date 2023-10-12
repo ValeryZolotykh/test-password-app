85c6106e00ede6fbc572dbb80abbe788724da755
Valeriia Zolotykh <98551132+ValeryZolotykh@users.noreply.github.com>
2023-10-12 11:44:57 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: '', component: AppComponent },
+];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
+  imports: [RouterModule.forRoot(routes),BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
+  exports: [RouterModule]
 })
 export class AppModule {}
